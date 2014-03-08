e5107a4b4390da253234b1a205f1aa1ea2b969e9
spiffytech <spiffytech@gmail.com>
2014-03-08 16:45:24 +0000

    Swapped simplex noise libraries. Tweaked output formatting to actually look sorta convincing.

--- webserver.py
+++ webserver.py
@@ -6,19 +6,22 @@ app = Flask(__name__, static_path="/static")
 import math
 
 from lib import simplexnoise as sn
+import noise
 @app.route("/sample_noise")
 def sample_noise():
     print "Generating noise"
     max_x = 1200
     max_y = 600
-    smoothness=.005
+    octaves=5
+    persistence=.25
+    scale=1
+    smoothness=.003
+    f=simplex2
     grid = tuple(
-        tuple(sn.scaled_octave_noise_2d(
-            octaves=2,
-            persistence=.125,  # amplitude
-            scale=1,  # frequency
-            loBound=0,
-            hiBound=255,
+        tuple(f(
+            octaves=4,
+            persistence=.5,  # amplitude
+            scale=2,  # frequency
             x=x*smoothness,
             y=y*smoothness
         ) for y in xrange(max_y))
@@ -27,6 +30,24 @@ def sample_noise():
             
     print "Returning noise"
     return Response(json.dumps(dict(grid=grid)), mimetype="application/json")
+
+def simplex1(octaves, persistence, scale, x, y):
+        return sn.scaled_octave_noise_2d(
+            octaves=octaves,
+            persistence=persistence,  # amplitude
+            scale=scale,  # frequency
+            loBound=0,
+            hiBound=255,
+            x=x*smoothness,
+            y=y*smoothness
+        )
+
+def simplex2(octaves, persistence, scale, x, y):
+    return noise.snoise2(x, y, octaves, persistence, scale)
+    return cosine_interpolation(0, 255, noise.snoise2(x, y, octaves, persistence, scale))
+
+def linear_interpolation(a, b, x):
+    return  a*(1-x) + b*x
 
 def cosine_interpolation(a, b, x):
     ft = x * 3.1415927
