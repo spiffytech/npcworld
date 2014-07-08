640e42a13dbdd9d4f0414466cbc96105c3a715e8
spiffytech <spiffytech@gmail.com>
2014-07-07 22:53:50 -0400

    FAKE script now compiles the app. Doesn't put binary/libraries in the right places, though.

---
+++ build.fsx
@@ -0,0 +1,41 @@
+//include Fake lib
+#r @"tools/FAKE.3.1.2/tools/FakeLib.dll"
+open Fake
+open Fake.FscHelper
+
+let buildDir = "./build"
+
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+
+// Default target
+Target "Default" (fun _ ->
+    trace "Hello World from FAKE"
+    ["main.fs"]
+    |> Fsc (fun parameters ->
+        {parameters with References = ["packages/simplexnoise_1_01/SimplexNoise/bin/Debug/SimplexNoise.dll"]}
+    )
+)
+
+Target "BuildApp" (fun _ ->
+    ()
+)
+
+Target "MakeDeps" (fun _ ->
+    !! "src/app/**/*.csproj"
+        |> MSBuildRelease buildDir "Build"
+        |> Log "AppBuild-Output: "
+)
+
+// Dependencies
+
+"Default"
+    ==> "MakeDeps"
+
+"Clean"
+    ==> "BuildApp"
+    ==> "Default"
+
+// start build
+RunTargetOrDefault "Default"
